9eda7bfb7d1cd607f2c363f1002773948c4d3853
Javier Kipen <kipen@kth.se>
2023-09-19 11:39:22 +0200

    Results with brownian show improvement on quipunet extended!

--- scripts/QuipuTrainWithES.py
+++ scripts/QuipuTrainWithES.py
@@ -4,13 +4,13 @@ from ModelFuncs import get_quipu_model
 import math
 
 physical_devices = tf.config.list_physical_devices('GPU')
-tf.config.set_visible_devices(physical_devices[2], 'GPU')
+tf.config.set_visible_devices(physical_devices[0], 'GPU')
 
 
 n_runs=100;
-n1=512;n2=512;
+n1=2048;n2=1024;
 brow_aug=0.9;
-use_brow_aug=False;
+use_brow_aug=True;
 
 lr=5e-4;batch_size=256; #Should keep them constant for all the runs to make a fair comparison
 
@@ -18,7 +18,7 @@ folder_es_train="../results/TrainingWithES/";
 mt=ModelTrainer(brow_std=brow_aug,batch_size=batch_size,brow_aug_use=use_brow_aug,lr=lr);
 model=get_quipu_model(n_dense_1=n1,n_dense_2=n2);
 if use_brow_aug:
-    run_name="WBrowAug_"+str(int(brow_aug))+str(int(math.modf(brow_aug)[0]*100))+"_N1_"+str(n1)+"_N2_"+str(n2)+".csv";
+    run_name="WBrowAug_"+str(int(brow_aug))+str(int(math.modf(brow_aug)[0]*100))+"_N1_"+str(n1)+"_N2_"+str(n2)+"_again.csv";
 else:
-    run_name="Reproduction"+"_N1_"+str(n1)+"_N2_"+str(n2)+".csv";
+    run_name="Reproduction"+"_N1_"+str(n1)+"_N2_"+str(n2)+"_again.csv";
 mt.crossval_es(model,n_runs=n_runs,data_folder=folder_es_train+run_name)
